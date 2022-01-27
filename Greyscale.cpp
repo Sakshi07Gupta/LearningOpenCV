34cdaad1f485cae238b1a166e1d30c3336cd8011
Sakshi Gupta <98098620+Sakshi07Gupta@users.noreply.github.com>
2022-01-27 17:31:06 +0530

    Add files via upload

---
+++ Greyscale.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<opencv2/opencv.hpp>
+#include <opencv2/core.hpp> // basic building blocks
+#include <opencv2/imgcodecs.hpp> // capabilities for reading and writing images 
+#include <opencv2/highgui.hpp> // view an image in a window
+#include <opencv2/imgproc.hpp> // for cvtColor
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char* argv)
+{
+		Mat img;
+	// load the image
+	img = imread("E:\\OPENCVr\\opencv\\Task2\\picture.jpg", IMREAD_COLOR);
+
+	Mat gray_img;
+	cvtColor(img, gray_img, COLOR_BGR2GRAY);
+
+	Mat binary_img;
+	cvtColor(gray_img, binary_img, THRESH_BINARY);
+
+	// imwrite(argv[1], gray_img);
+	imshow("color image", img);
+	 imshow("gray image", gray_img);
+	 imshow("binary image", binary_img);
+	waitKey(0);
+	destroyAllWindows();
+
+	return 0;
+}
+
+
