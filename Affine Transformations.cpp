e97452e44963b93bdb286ae3a3c2f09fe0476852
Sakshi Gupta <98098620+Sakshi07Gupta@users.noreply.github.com>
2022-01-28 17:18:17 +0530

    Add files via upload

---
+++ Affine Transformations.cpp
@@ -0,0 +1,36 @@
+/*#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp" 
+#include<iostream>
+using namespace cv;
+using namespace std;
+int main() {
+
+	Mat image;
+
+	image	= imread("E:\\OPENCVr\\opencv\\Chapter1\\thor.jpg", IMREAD_COLOR);
+	Mat output_image = Mat::zeros(Size(image.cols, image.rows), image.type());
+	namedWindow("original image", 0);
+	namedWindow("Output Image",0 );
+
+	Point2f inpMat[3];
+	Point2f outMat[3];
+	inpMat[0] = Point2f(0.0,0.0);
+	inpMat[1] = Point2f(image.cols,0);
+	inpMat[2] = Point2f(0, image.rows);
+
+	outMat[0] = Point2f(0,200);
+	outMat[1] = Point2f(500,100);
+	outMat[2] = Point2f(170,500);
+
+	Mat M =getAffineTransform(inpMat, outMat);
+	warpAffine(image, output_image, M, output_image.size());
+
+	cout << M << endl;
+
+	imshow("original image", image);
+	imshow("output image", output_image);
+	waitKey(0);
+	return 0;
+
+}*/
