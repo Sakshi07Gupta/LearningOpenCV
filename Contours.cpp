d67a76e86c92b4951e79a73efb4daf94a2b0d540
Sakshi Gupta <98098620+Sakshi07Gupta@users.noreply.github.com>
2022-01-31 18:05:22 +0530

    Add files via upload

--- Contours.cpp
+++ Contours.cpp
@@ -1,29 +1,51 @@
-/*#include<iostream>
-#include<opencv2/highgui.hpp>
-#include<opencv2/imgproc.hpp>
-#include<opencv2/highgui/highgui.hpp>
-using namespace cv;
+/*#include<opencv2/opencv.hpp>
+#include <iostream>
 using namespace std;
-int main()
-{
-	Mat zeros;
-	Mat frame = Mat:: zeros(Size(1000, 1000), CV_8UC3);
-	namedWindow("output", 0);
+using namespace cv;
 
-	std::vector < std::vector<cv::Point>> contours = 
-	{
-		{cv::Point(100,100), cv::Point(100,150), cv::Point(150,150)},
-		{cv::Point(300,300), cv::Point(400,400)},
-		{cv::Point(100,300), cv::Point(200,400)},
-		{cv::Point(200,300), cv::Point(0,300), cv::Point(450,550), cv::Point(600,800)},
-		{cv::Point(100,250), cv::Point(80,340), cv::Point(500,750), cv::Point(900,950),cv::Point(990,930)}  
+int main() {
 
-	};
-	for (int i = 0; i < contours.size(); i++)
-		drawContours(frame, contours, i, Scalar(0, 255, 45), 5);
+	// read the image
 
-	imshow("output", frame);
+	Mat image = imread("E:\\OPENCVr\\opencv\\Task24\\building.jpg");
+	Mat img_gray;
+	cvtColor(image, img_gray, COLOR_BGR2GRAY);
+	Mat thresh;
+	threshold(img_gray, thresh, 140, 255, THRESH_BINARY);
+	imshow("Binary mage", thresh);
 	waitKey(0);
-	return 0;
+	imwrite("image_thres1.jpg", thresh);
 
+	destroyAllWindows();
+	// detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+
+	vector<vector<Point>> contours;
+
+	vector<Vec4i> hierarchy;
+
+	findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+	// here thresh is the image in binary form 
+	// RETR_TREE is the mode of contour retrieval 
+	// CHAIN_APPROX_NONE is the method of contour approximation
+	// draw contours on the original image
+
+	Mat image_copy = image.clone();
+	// image_cpy is the image 
+    // -1 is the contourIndex
+	// (0,255,0) is the color code
+	// line type = LINE_AA
+	// thickness= 2
+	drawContours(thresh, contours, -1, Scalar(0, 23, 200), 3);
+	drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
+
+	imshow("contours", image_copy);
+	imshow("Contours1", thresh);
+	
+
+	waitKey(0);
+
+	imwrite("contours_none_image1.jpg", image_copy);
+	imwrite("contours_none_image1.jpg", thresh);
+
+	destroyAllWindows();
 }*/
